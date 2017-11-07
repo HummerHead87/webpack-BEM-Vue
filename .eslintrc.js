6fed33b516f56284e2a7a4c1157deaf4f04bca70
HummerHead87 <snooks87@gmail.com>
2017-11-07 12:50:38 +0300

    modify .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,37 @@
 module.exports = {
-    "extends": "airbnb-base"
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'webpack.config.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-undef': 0
+  }
 };
